ef1445e65db1e7c8cb5bb76ac90a35abc3e6bcc2
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2024-09-23 17:16:40 +0330

    adding last scripts

--- js/pageAnime.js
+++ js/pageAnime.js
@@ -1,4 +1,21 @@
+addEventListener("DOMContentLoaded", function () {
+  const main = this.document.querySelector(".main");
+  const outerNav = this.document.querySelector(".outer--nav");
+  gsap.to(".main", {
+    opacity: 1,
+    duration: 0.5,
+    ease: "expo.out",
+    onComplete: function () {
+      main.classList.remove("opacity-0");
+      outerNav.classList.remove("opacity-0");
+    },
+  });
+});
+
 const navLinks = document.querySelectorAll(".nav--links");
+const pageHandlerLinks = document.querySelectorAll(
+  ".side--nav button, .outer--nav li"
+);
 const isTouchSupported = "ontouchstart" in document.documentElement;
 let isMovingAllowed = true;
 let pageData = {
@@ -37,6 +54,17 @@ if (isTouchSupported) {
   });
 }
 
+pageHandlerLinks.forEach((member) => {
+  member.addEventListener("click", function (e) {
+    const btnNum = Number(member.getAttribute("btnNum"));
+    const lastPage = pageData.currentPage;
+    setStyle("jump", btnNum);
+    updatePageNumbers("jump", btnNum);
+    animateSideNav("jump", lastPage);
+    updateActivePage();
+  });
+});
+
 function goNextPage() {
   setStyle("next");
   updatePageNumbers("next");
@@ -51,8 +79,9 @@ function goPreviousPage() {
   updateActivePage();
 }
 
-function setStyle(status) {
+function setStyle(status, btnNum) {
   const tl = gsap.timeline();
+  const newPage = status === "next" ? pageData.nextPage : pageData.prevPage;
 
   tl.to(`.page__num__${pageData.currentPage}`, {
     y: "-=50px",
@@ -62,53 +91,62 @@ function setStyle(status) {
     onStart: () => {
       isMovingAllowed = false;
     },
+    onComplete: () => {
+      pageContainer.classList.remove("perspectivePage");
+    },
   })
     .set(`.page__num__${pageData.currentPage}`, {
       y: 0,
       zIndex: "-1",
       opacity: 1,
     })
-    .set(
-      `.page__num__${
-        status === "next" ? pageData.nextPage : pageData.prevPage
-      }`,
-      {
-        zIndex: "1",
-        opacity: 0,
-      }
-    )
-    .from(
-      `.page__num__${
-        status === "next" ? pageData.nextPage : pageData.prevPage
-      }`,
-      {
-        y: "+=50px",
-        duration: 0.3,
-        opacity: "1",
-        onComplete: () => {
-          isMovingAllowed = true;
-        },
-      }
-    );
+    .set(`.page__num__${status === "jump" ? btnNum : newPage}`, {
+      zIndex: "1",
+      opacity: 0,
+    })
+    .from(`.page__num__${status === "jump" ? btnNum : newPage}`, {
+      y: "+=50px",
+      duration: 0.3,
+      opacity: "1",
+      onComplete: () => {
+        isMovingAllowed = true;
+      },
+    });
 }
 
-function increaseNum(vairable) {
-  if (pageData[vairable] === 5) {
-    pageData[vairable] = 1;
+function increaseNum(vairable, sendValue = false, btnNum) {
+  if (!sendValue) {
+    if (pageData[vairable] === 5) {
+      pageData[vairable] = 1;
+    } else {
+      pageData[vairable]++;
+    }
   } else {
-    pageData[vairable]++;
+    if (btnNum === 5) {
+      return 1;
+    } else {
+      return btnNum + 1;
+    }
   }
 }
 
-function decreaseNum(vairable) {
-  if (pageData[vairable] === 1) {
-    pageData[vairable] = 5;
+function decreaseNum(vairable, sendValue = false, btnNum) {
+  if (!sendValue) {
+    if (pageData[vairable] === 1) {
+      pageData[vairable] = 5;
+    } else {
+      pageData[vairable]--;
+    }
   } else {
-    pageData[vairable]--;
+    if (btnNum === 1) {
+      return 5;
+    } else {
+      return btnNum - 1;
+    }
   }
 }
 
-function updatePageNumbers(status) {
+function updatePageNumbers(status, btnNum) {
   if (status === "next") {
     // updating data in next status
     increaseNum("prevPage");
@@ -119,11 +157,16 @@ function updatePageNumbers(status) {
     decreaseNum("prevPage");
     decreaseNum("currentPage");
     decreaseNum("nextPage");
+  } else if (status === "jump") {
+    pageData.prevPage = decreaseNum(null, true, btnNum);
+    pageData.currentPage = btnNum;
+    pageData.nextPage = increaseNum(null, true, btnNum);
   }
 }
 
-function animateSideNav(status) {
+function animateSideNav(status, lastJumpedPage) {
   const tl = gsap.timeline();
+  const lastPage = status === "next" ? pageData.prevPage : pageData.nextPage;
 
   tl.to(
     `.pageNav-num-${pageData.currentPage} .number`,
@@ -153,9 +196,7 @@ function animateSideNav(status) {
       "startTogether"
     )
     .to(
-      `.pageNav-num-${
-        status === "next" ? pageData.prevPage : pageData.nextPage
-      } .number`,
+      `.pageNav-num-${status === "jump" ? lastJumpedPage : lastPage} .number`,
       {
         x: "0",
         color: "#555555",
@@ -165,9 +206,7 @@ function animateSideNav(status) {
       "startTogether"
     )
     .to(
-      `.pageNav-num-${
-        status === "next" ? pageData.prevPage : pageData.nextPage
-      } .ball`,
+      `.pageNav-num-${status === "jump" ? lastJumpedPage : lastPage} .ball`,
       {
         scale: 1,
         duration: 0.5,
@@ -175,9 +214,7 @@ function animateSideNav(status) {
       "startTogether"
     )
     .to(
-      `.pageNav-num-${
-        status === "next" ? pageData.prevPage : pageData.nextPage
-      } .label`,
+      `.pageNav-num-${status === "jump" ? lastJumpedPage : lastPage} .label`,
       {
         opacity: 0,
         color: "#555555",
@@ -189,7 +226,7 @@ function animateSideNav(status) {
 function updateActivePage() {
   const currentActivePage = document.querySelector(".active--page");
   currentActivePage.classList.remove("active--page");
-  navLinks[pageData.currentPage - 1].classList.add("active--page")
+  navLinks[pageData.currentPage - 1].classList.add("active--page");
 }
 
 // perspective animation
